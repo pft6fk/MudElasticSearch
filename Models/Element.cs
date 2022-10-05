b33b778dfc1f988263acc3351f9d7a0a8784a617
unnam <unnam@MUHAMMADAZIZ>
2022-10-06 00:47:48 +0500

    Add project files.

---
+++ Models/Element.cs
@@ -0,0 +1,11 @@
+﻿namespace Solr_test.Models
+{
+    public class Element
+    {
+        public int Number { get; set; }
+        public string Sign { get; set; }
+        public string Name { get; set; }
+        public int Position { get; set; }
+        public double Molar { get; set; }
+    }
+}
